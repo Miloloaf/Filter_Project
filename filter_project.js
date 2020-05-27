c5c41046e4e7338ec99b56ae6fdedbedebca1d51
Miloloaf <Miloloaf@users.noreply.github.com>
2020-05-27 22:08:49 +0100

    0.5

--- filter_project.js
+++ filter_project.js
@@ -1,5 +1,5 @@
 // const aboutUsImg = document.getElementById("aboutUsImage");
-let itemNumber = 2
+let itemNumber = 1
 
 function Food (url, name, price, x, y, type) {
     this.url = url;
@@ -12,13 +12,41 @@ function Food (url, name, price, x, y, type) {
     itemNumber += 1; 
 }
 
-
+let item1 = new Food("https://images.unsplash.com/photo-1551059429-355bd51f6972?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80",
+"Cake Item", "£4", 0, 0 , "Cake");
 
 let item2 = new Food("https://images.unsplash.com/photo-1550617931-e17a7b70dce2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
 "Cupcake Item", "£10", 0, 0, "Cupcake");
 
 let item3 = new Food ("https://images.unsplash.com/photo-1509474458352-109268893243?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
 "Doughnut Item", "£7.50", 0, 0, "Doughnut");
+
+let item4 = new Food ("https://images.unsplash.com/photo-1568051243858-533a607809a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+"Cookie Item", "£5", 0, 0, "Cookie");
+
+let item5 = new Food ("https://images.unsplash.com/photo-1453499988014-b7b65cc79c13?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+"Cupcake Item", "£6", 0, 0, "Cupcake");
+
+let item6 = new Food ("https://images.unsplash.com/photo-1533134242443-d4fd215305ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+"Cake Item", "£7", 0, 0, "Cake");
+
+let item7 = new Food ("https://cdn.pixabay.com/photo/2014/02/17/15/34/donuts-268250_960_720.jpg",
+"Doughnut Item", "£4", 0, 0, "Doughnut");
+
+let item8 = new Food ("https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg",
+"Cake Item", "£6", 0, 0, "Cake");
+
+let item9 = new Food ("https://images.unsplash.com/photo-1563076429-c04cbe68da3a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+"Cupcake Item", "£5", 0, 0, "Cupcake")
+
+let item10 = new Food ("https://images.unsplash.com/photo-1583182332473-b31ba08929c8?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+"Doughnut Item", "£8", 0, 0, "Doughnut");
+
+let item11 = new Food ("https://images.unsplash.com/photo-1544883930-924db8d63597?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+"Cookie Item", "£5", 0, 0, "Cookie")
+
+let item12 = new Food ("https://cdn.pixabay.com/photo/2019/06/13/14/13/cheesecake-4271724_960_720.jpg",
+"Cake Item", "£7", 0, 0, "Cake")
 
 function moveBorder() {
     document.getElementById("aboutUsImageBorder").classList.add("static")
@@ -81,13 +109,31 @@ function testGridText () {
 
 
 function populateInventory() {
-    createCard(item2);
-    createCard(item3);
+    for (let i = 1; i < itemNumber; i++){
+        createCard(eval("item"+i));
+    }
 }
+let foodImages = document.getElementsByClassName("foodBackground");
 
 function test(){
-    document.body.addEventListener("click", () => {alert("Test");})
+    // let foodImages = document.querySelectorAll(".foodBackground");
+    document.body.addEventListener("click", () => {alert(foodImages.length);})
+}
+
+function zoomImages() {
+    // let target = event.target
+    event.target.style.backgroundSize = "800px";
+
 }
 
 document.getElementById("aboutUsImage").addEventListener("mouseenter", moveBorder);
 document.getElementById("aboutUsImage").addEventListener("mouseleave", removeBorder);
+
+
+function addFoodImageEvents () {
+    for (let i = 0; i < foodImages.length; i++){
+    foodImages[i].addEventListener("click", zoomImages )
+}
+
+
+}
